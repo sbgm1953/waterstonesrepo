0bdd02b5f2fc2696bd1c5704ebb1113a331c49d2
Salma Begum <begums731@gmail.com>
2022-03-01 16:03:17 +0000

    initial commit

---
+++ waterstones/src/main/java/com/qa/waterstone/WaterstonesApplication.java
@@ -0,0 +1,13 @@
+package com.qa.waterstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterstonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterstonesApplication.class, args);
+	}
+
+}
